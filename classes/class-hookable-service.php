ec6e93cd60f4f1efaf69027b4759944417b8e2f7
tmf <tom.forrer@gmail.com>
2014-11-13 14:12:38 +0100

    HookableService base class
    nothing special, just aware of the container, and an default entry
    point method

---
+++ classes/class-hookable-service.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @autor Tom Forrer <tom.forrer@gmail.com>
+ * @copyright Copyright (c) 2014 Tom Forrer (http://github.com/tmf)
+ */
+
+namespace Tmf\Container;
+
+use Pimple\Container;
+
+/**
+ * Class HookableService
+ *
+ * @package Tmf\Container
+ */
+abstract class HookableService implements ContainerAwareInterface
+{
+  /**
+   * @var Container $container the pimple service container
+   */
+  protected $container;
+
+  /**
+   * By default the HookableServiceProvider calls the "initialize" method when the 'init' WordPress action is triggered.
+   * Override this method in your service, or define your own entry point when registering a HookableServiceProvider.
+   */
+  public function initialize()
+  {
+
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function getContainer()
+  {
+    return $this->container;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function setContainer(Container $container)
+  {
+    $this->container = $container;
+  }
+}
