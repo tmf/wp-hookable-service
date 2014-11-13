6d0909c95154f2403a344e4ec285d643e9c83701
tmf <tom.forrer@gmail.com>
2014-11-13 14:10:49 +0100

    ContainerAware interface
    an interface for setting the pimple container (i.e. for a service class)

---
+++ classes/interface-container-aware.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @autor Tom Forrer <tom.forrer@gmail.com>
+ * @copyright Copyright (c) 2014 Tom Forrer (http://github.com/tmf)
+ */
+
+namespace Tmf\Container;
+
+use Pimple\Container;
+
+/**
+ * Interface ContainerAwareInterface
+ *
+ * A Pimple container dependency can be injected to classes implementing this interface.
+ *
+ * @package Tmf\Container
+ */
+interface ContainerAwareInterface
+{
+  /**
+   * Getter
+   *
+   * @return Container the pimple service container
+   */
+  public function getContainer();
+
+  /**
+   * Setter
+   *
+   * @param Container $container the pimple service container
+   */
+  public function setContainer(Container $container);
+} 
