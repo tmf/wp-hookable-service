7e133bb406c77c6bb2b56a491a3965a912765a1d
tmf <tom.forrer@gmail.com>
2014-11-15 17:21:47 +0100

    change initializationMethods to hookConnections
    hook connections are arrays of ['hook' => .., 'method' => …, priority =
    10] definitions

--- classes/class-hookable-service-provider.php
+++ classes/class-hookable-service-provider.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @autor Tom Forrer <tom.forrer@gmail.com>
+ * @autor     Tom Forrer <tom.forrer@gmail.com>
  * @copyright Copyright (c) 2014 Tom Forrer (http://github.com/tmf)
  */
 
@@ -16,92 +16,95 @@ use Pimple\ServiceProviderInterface;
  */
 class HookableServiceProvider implements ServiceProviderInterface
 {
-  /**
-   * @var string the key for the service in the pimple service container
-   */
-  protected $serviceKey;
-  /**
-   * @var string the fully qualified class name for the service factory method
-   */
-  protected $serviceClass;
-  /**
-   * @var array an array of (public) service methods names indexed by hook name. i.e. array('init' => 'initialize')
-   */
-  protected $initializeMethodsByHook = array();
+    /**
+     * @var string the key for the service in the pimple service container
+     */
+    protected $serviceKey;
+    /**
+     * @var string the fully qualified class name for the service factory method
+     */
+    protected $serviceClass;
+    /**
+     * @var array an array of (public) service methods names indexed by hook name. i.e. array('init' => 'initialize')
+     */
+    protected $hookConnections = array();
 
-  /**
-   * Construct a HookableServiceProvider with all needed parameters.
-   * By default, the entry point (the point where the service is at least instantiated by the container)
-   * during the request lifecycle is the 'init' action.
-   *
-   * @param string $serviceKey a key for the container
-   * @param string $serviceClass the fully qualified class name of the service (should extend the
-   *                                        HookableService class, but can be any class)
-   * @param array $initializeMethodsByHook a list of entry points: each value containing a method name of the
-   *                                        service will get called, when the wordpress filter/action specified in
-   *                                        the key are triggered.
-   *                                        By default, 'initialize' is called when 'init' is triggered
-   */
-  public function __construct($serviceKey, $serviceClass, $initializeMethodsByHook = array('init' => 'initialize'))
-  {
-    $this->serviceKey = $serviceKey;
-    $this->serviceClass = $serviceClass;
-    $this->initializeMethodsByHook = $initializeMethodsByHook;
-  }
+    /**
+     * Construct a HookableServiceProvider with all needed parameters.
+     * By default, the entry point (the point where the service is at least instantiated by the container)
+     * during the request lifecycle is the 'init' action.
+     *
+     * @param string $serviceKey              a key for the container
+     * @param string $serviceClass            the fully qualified class name of the service (should extend the
+     *                                        HookableService class, but can be any class)
+     * @param array  $hookConnections         a list of entry points: each value containing a method name of the
+     *                                        service will get called, when the wordpress filter/action specified in
+     *                                        the key are triggered.
+     *                                        By default, 'initialize' is called when 'init' is triggered
+     */
+    public function __construct($serviceKey, $serviceClass, $hookConnections = [['hook' => 'init', 'method' => 'initialize', 'priority' => 10]])
+    {
+        $this->serviceKey = $serviceKey;
+        $this->serviceClass = $serviceClass;
 
-  /**
-   * Register a HookableService (or any kind of class) as a service, where when the wordpress hook callback is called,
-   * the service gets instantiated and gets called with a specified initalization method.
-   *
-   * @param Container $container
-   */
-  public function register(Container $container)
-  {
-    $serviceKey = $this->serviceKey;
-    $serviceClassKey = $this->serviceKey . '.class';
-
-    // set the service class parameter in the container
-    $container[$serviceClassKey] = $this->serviceClass;
-
-    // set service instantiation function
-    $container[$serviceKey] = function ($container) use ($serviceClassKey) {
-      // instantiate the class from the class parameter
-      $service = new $container[$serviceClassKey]();
-
-      // inject the container, if supported
-      if ($service instanceof ContainerAwareInterface) {
-        $service->setContainer($container);
-      }
-
-      return $service;
-    };
-
-    // add actions and filters to request, exactly when needed, the service from the container
-    foreach ($this->initializeMethodsByHook as $hook => $method) {
-      $this->hookService($hook, $method, $container, $serviceKey);
+        if (!isset($hookConnections['priority'])) {
+            $hookConnections['priority'] = 10;
+        }
+        $this->hookConnections = $hookConnections;
     }
-  }
 
-  /**
-   * @param string $hook the WordPress hook, an action or a filter
-   * @param string $method the method to hook up
-   * @param object $container the Pimple container
-   * @param string $serviceKey
-   */
-  protected function hookService($hook, $method, $container, $serviceKey)
-  {
-    // always register with priority 10
-    $priority = 10;
+    /**
+     * Register a HookableService (or any kind of class) as a service, where when the wordpress hook callback is called,
+     * the service gets instantiated and gets called with a specified initalization method.
+     *
+     * @param Container $container
+     */
+    public function register(Container $container)
+    {
+        $serviceKey = $this->serviceKey;
+        $serviceClassKey = $this->serviceKey . '.class';
 
-    // use an anonymous function to "use" the $container $serviceKey and $method objects/values
-    add_filter($hook, function () use ($container, $serviceKey, $method) {
+        // set the service class parameter in the container
+        $container[$serviceClassKey] = $this->serviceClass;
 
-      // call the specified method with all arguments provided by the action / filter, only when present
-      if (method_exists($container[$serviceKey], $method)) {
+        // set service instantiation function
+        $container[$serviceKey] = function ($container) use ($serviceClassKey) {
+            // instantiate the class from the class parameter
+            $service = new $container[$serviceClassKey]();
 
-        // if the $param_arr array has more elements than the method has arguments: it works fine. more method arguments than elements in $param_arr triggers an error
-        call_user_func_array(array($container[$serviceKey], $method), func_get_args());
-      }
-    }, $priority, 100); // 100 should be enough accepted arguments. call_user_func_array will take care of the rest
-  }
+            // inject the container, if supported
+            if ($service instanceof ContainerAwareInterface) {
+                $service->setContainer($container);
+            }
+
+            return $service;
+        };
+
+        // add actions and filters to request, exactly when needed, the service from the container
+        foreach ($this->hookConnections as $hookConnection) {
+            $this->hookService($hookConnection['hook'], $hookConnection['method'], $hookConnection['priority'], $container, $serviceKey);
+        }
+    }
+
+    /**
+     * @param string $hook      the WordPress hook, an action or a filter
+     * @param string $method    the method to hook up
+     * @param int    $priority  the action's or filter's priority
+     * @param object $container the Pimple container
+     * @param string $serviceKey
+     */
+    protected function hookService($hook, $method, $priority, $container, $serviceKey)
+    {
+
+        // use an anonymous function to "use" the $container $serviceKey and $method objects/values
+        add_filter($hook, function () use ($container, $serviceKey, $method) {
+
+            // call the specified method with all arguments provided by the action / filter, only when present
+            if (method_exists($container[$serviceKey], $method)) {
+
+                // if the $param_arr array has more elements than the method has arguments: it works fine. more method arguments than elements in $param_arr triggers an error
+                call_user_func_array(array($container[$serviceKey], $method), func_get_args());
+            }
+        }, $priority, 100); // 100 should be enough accepted arguments. call_user_func_array will take care of the rest
+    }
 } 
